f3f3a5ad54f125efe5085510206a35c547429965
Ana C. Perez-Gea <acpg@users.noreply.github.com>
2015-01-25 18:14:20 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,53 +1,35 @@
-# Source of data for this project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+library(plyr)
+setwd("~/Dropbox/COURSERA/DATA SCIENCE/03 Getting and Cleaning Data/Project")
+setwd('UCI HAR Dataset')
 
-# This R script does the following:
-
-# 1. Merges the training and the test sets to create one data set.
+# Merges the training and the test sets to create one data set.
 
 X <- rbind(read.table("train/X_train.txt"), read.table("test/X_test.txt"))
-
 subj <- rbind(read.table("train/subject_train.txt"), read.table("test/subject_test.txt"))
-
 Y <- rbind(read.table("train/y_train.txt"), read.table("test/y_test.txt"))
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# Extracts only the measurements on the mean and standard deviation for each measurement.
 
 features <- read.table("features.txt")
-indices <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, indices]
-names(X) <- features[indices, 2]
-names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X))
+X <- X[ , grep("-mean\\(\\)|-std\\(\\)", features[, 2])]
+features <- features[grep("-mean\\(\\)|-std\\(\\)", features[, 2]), 2]
+names(X) <- gsub("\\(|\\)", "", features)
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+# Uses descriptive activity names to name the activities in the data set.
 
 activities <- read.table("activity_labels.txt")
-activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
 
-# 4. Appropriately labels the data set with descriptive activity names.
+# Appropriately labels the data set with descriptive activity names.
 
 names(subj) <- "subject"
-cleaned <- cbind(subject, Y, X)
-write.table(cleaned, "merged_data.txt")
+dat <- cbind(subj, Y, X)
 
-# 5. Creates a second, independent data set with the average of each variable for each activity and each subject.
+# Creates a second, independent data set with the average of each variable for each activity and each subject.
 
-uniqueSubjects = unique(subj)[,1]
-numSubjects = length(unique(subj)[,1])
-numActivities = length(activities[,1])
-numCols = dim(cleaned)[2]
-result = cleaned[1:(numSubjects*numActivities), ]
+aux <- aggregate(x = dat[, 3:ncol(dat)], 
+                 by = list(subject = dat$subject, activity= dat$activity), 
+                 FUN = "mean", na.rm = T)
 
-row = 1
-for (i in 1:numSubjects) {
-  for (j in 1:numActivities) {
-    result[row, 1] = uniqueSubjects[i]
-    result[row, 2] = activities[j, 2]
-    tmp <- cleaned[cleaned$subject == i & cleaned$activity == activities[j, 2], ]
-    result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
-    row = row+1
-  }
-}
-write.table(result, "data_averages.txt")
+write.table(aux, "data_averages.txt", row.name = FALSE)
